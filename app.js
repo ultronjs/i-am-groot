bb9b2d2abdc45512fb4e357196b43de10de8a44d
ultron07 <50156573+ultron07@users.noreply.github.com>
2021-03-22 18:03:28 +0530

    js completed

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("i am working")
+var inputBox = document.querySelector(".input-box")
+var translateButton = document.querySelector(".btn-translate")
+var outputBox = document.querySelector(".output-box")
+var testUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var url="https://api.funtranslations.com/translate/groot.json"
+
+function urlToFetch(inputText){
+    var toFetch = url + "?text="+ inputText;
+    encoded = encodeURI(toFetch);
+    return encoded
+}
+
+
+translateButton.addEventListener("click",translateText)
+
+function translateText(){
+    console.log("I m fired")
+    var inputText = inputBox.value;
+    console.log(decodeURI(urlToFetch(inputText)))
+    fetch(decodeURI(urlToFetch(inputText)))
+    .then(response => response.json())
+    .then(json => outputBox.innerText = json.contents.translated)
+}
